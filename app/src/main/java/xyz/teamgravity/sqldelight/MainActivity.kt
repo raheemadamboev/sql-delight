026191e28bac0e96e652410cc6e9d0d1ca6e0f97
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-04 14:36:41 +0500

    dependencies setup

--- app/src/main/java/xyz/teamgravity/sqldelight/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/sqldelight/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import xyz.teamgravity.sqldelight.ui.theme.SQLDelightTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +14,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SQLDelightTheme {
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
                 }
             }
         }
